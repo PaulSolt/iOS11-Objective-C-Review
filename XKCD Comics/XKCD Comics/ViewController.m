420e37ed944292b695dc88fa04c90bac7d636349
Paul Solt <PaulSolt@gmail.com>
2020-01-30 14:42:15 -0500

    Working image request

--- XKCD Comics/XKCD Comics/ViewController.m
+++ XKCD Comics/XKCD Comics/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 #import "LSILog.h"
+#import "LSIErrors.h"
+#import "Comic.h"
 
 @interface ViewController ()
 
@@ -39,16 +41,71 @@
             // TODO: Completion
         }
         
+        if (!data) {
+            NSError *dataError = errorWithMessage(@"Data is missing", LSIDataNilError);
+            NSLog(@"Error: %@", dataError);
+            // TODO: Completion
+        }
+        
         NSError *jsonError = nil;
         NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
         
         NSLog(@"JSON: %@", json);
 
+        Comic *comic = [[Comic alloc] initWithDictionary:json];
+        
+        NSLog(@"Comic: %@", comic.imageURL);
+        
+        [self downloadImage:comic.imageURL];
+        
+//        [self downloadImage:[NSURL URLWithString:@"https://apod.nasa.gov/apod/image/2001/C2017T2_2020-01-24-26-28_posta.jpg"]];
+        
+        NSURL *testImageURL = [[NSBundle mainBundle] URLForResource:@"comic" withExtension:@"png"];
+        NSLog(@"testImage; %@", testImageURL);
         
         
+        NSData *data1 = [NSData dataWithContentsOfURL:testImageURL];
+        NSLog(@"data1: %@", data1);
+        UIImage *image = [UIImage imageWithContentsOfFile:testImageURL.path];
+        NSLog(@"image: %@", image);
+        
+        // Download image
+        // UI Update
+
         
     }] resume];
 }
 
+- (void)downloadImage:(NSURL *)url {
+    
+//    NSData *data = [NSData dataWithContentsOfURL:url];
+//    NSLog(@"Data2: %@", data);
+//    UIImage *image = [UIImage imageWithData:data];
+//    NSLog(@"Image: %@", image);
+
+    [[[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+
+        if (error) {
+            NSLog(@"Error: %@", error);
+            // TODO: Completion
+        }
+
+        if (!data) {
+            NSError *dataError = errorWithMessage(@"Data is missing", LSIDataNilError);
+            NSLog(@"Error: %@", dataError);
+            // TODO: Completion
+        }
+        
+        UIImage *image = [UIImage imageWithData:data];
+        NSLog(@"Image: %@", image);
+
+//        NSLog(@"Response: %@", response);
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+//            self.imageView.image = image;
+        });
+        
+    }] resume];
+}
 
 @end
