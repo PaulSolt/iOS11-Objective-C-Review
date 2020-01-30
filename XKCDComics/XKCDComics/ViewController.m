53f7b022a997aec1576f07328f97e21df6297c63
Dan M <djmthedev@gmail.com>
2020-01-30 10:31:33 -0800

    initial

---
+++ XKCDComics/XKCDComics/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  XKCDComics
+//
+//  Created by morse on 1/30/20.
+//  Copyright © 2020 morse. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
