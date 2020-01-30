d8d2870df8cc629d98ad5ce4e4cd87f1fd1a85f0
Paul Solt <PaulSolt@gmail.com>
2020-01-30 13:30:32 -0500

    Added starter

---
+++ XKCD Comics/XKCD Comics/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XKCD Comics
+//
+//  Created by Paul Solt on 1/30/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
