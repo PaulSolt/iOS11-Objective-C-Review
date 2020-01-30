3707bf548763137b883a5c06dfcd76298b9a6bbc
Paul Solt <PaulSolt@gmail.com>
2020-01-30 13:54:59 -0500

    It's alive. we have data from the API

--- XKCD Comics/XKCD Comics/ViewController.m
+++ XKCD Comics/XKCD Comics/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "LSILog.h"
 
 @interface ViewController ()
 
@@ -17,16 +18,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+//     Is there an interface for automated systems to access comics and metadata?
+//    Yes. You can get comics through the JSON interface, at URLs like http://xkcd.com/info.0.json (current comic) and http://xkcd.com/614/info.0.json (comic #614).
+    //    https://.xkcd.com/comics/worst_thing_that_could_happen.png
+    // http://xkcd.com/info.0.json = 2261
 
-    // What do we need to do?
+    NSURL *baseURL = [NSURL URLWithString:@"https://xkcd.com/"];
+//    NSURLComponents *urlComponents = [NSURLComponents componentsWithURL:baseURL resolvingAgainstBaseURL:YES];
+    
+    int comicNumber = 2261;
+    NSString *comicNumberString = [NSString stringWithFormat:@"%i", comicNumber];
+    NSURL *url = [[baseURL URLByAppendingPathComponent:comicNumberString] URLByAppendingPathComponent:@"info.0.json"];
 
-//    https://imgs.xkcd.com/comics/worst_thing_that_could_happen.png
 
-
-
-
-
-
+    [[[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        NSLog(@"Data!");
+        
+        
+        
+        
+    }] resume];
 }
 
 
